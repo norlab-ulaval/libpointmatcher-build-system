c11c050ffc03d8fef1117fc282ee3e3210316e54
RedLeaderOne <luc.coupal.1@ulaval.ca>
2023-07-29 16:00:15 -0400

    NMO-225 | Add LPM_IMAGE_ARCHITECTURE logic to lpm_install_libpointmatcher_ubuntu.bash;

--- build_system/lpm_execute_compose.bash
+++ build_system/lpm_execute_compose.bash
@@ -135,9 +135,9 @@ done
 #${MSG_END_FORMAT} "
 
 
-# ..................................................................................................................
-# Set environment variable LPM_IMAGE_ARCHITECTURE
-source ./lpm_utility_script/lpm_export_which_architecture.bash
+## ..................................................................................................................
+## Set environment variable LPM_IMAGE_ARCHITECTURE
+#source ./lpm_utility_script/lpm_export_which_architecture.bash
 
 # ..................................................................................................................
 print_msg "Executing docker compose command on ${MSG_DIMMED_FORMAT}docker-compose.libpointmatcher.yaml${MSG_END_FORMAT} with command ${MSG_DIMMED_FORMAT}${DOCKER_COMPOSE_CMD_ARGS}${MSG_END_FORMAT}"
