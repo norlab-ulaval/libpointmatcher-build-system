a3b36656c23ffb6fdb64993df27e18c0fba9a20b
RedLeaderOne <luc.coupal.1@ulaval.ca>
2023-08-04 15:53:56 -0400

    NMO-193 | Added TeamCity service message logic to improve readability by structuring build log steps:
    - Implemented a function which either start or finish TeamCity **compilation** service message
    - Implemented a function which either opened or closed TeamCity **bloc** service message
    - Refactored the lpm_install_dependencies_ubuntu.bash script with both service msg functions

--- build_system/function_library/general_utilities.bash
+++ build_system/function_library/general_utilities.bash
@@ -15,7 +15,6 @@ if [[ "$(basename $(pwd))" != "build_system" ]]; then
   exit 1
 fi
 
-
 # ....Project root logic...........................................................................................
 TMP_CWD=$(pwd)
 
@@ -29,7 +28,6 @@ set +o allexport
 # import shell functions from utilities library
 source ./function_library/prompt_utilities.bash
 
-
 # =================================================================================================================
 # Show docker command and execute it. Just enclose the command with argument in quote.
 #
@@ -41,21 +39,88 @@ source ./function_library/prompt_utilities.bash
 #
 # =================================================================================================================
 function show_and_execute_docker() {
-    local FULL_DOCKER_COMMAND=$1
-#    local MSG_DIMMED_FORMAT="\033[1;2m"
-#    local MSG_END_FORMAT="\033[0m"
+  local FULL_DOCKER_COMMAND=$1
+  #    local MSG_DIMMED_FORMAT="\033[1;2m"
+  #    local MSG_END_FORMAT="\033[0m"
 
-    if [ -f /.dockerenv ]; then
-      echo
-      print_msg_warning "Skipping the execution of Docker command\n
+  if [ -f /.dockerenv ]; then
+    echo
+    print_msg_warning "Skipping the execution of Docker command\n
       ${MSG_DIMMED_FORMAT}$ docker ${FULL_DOCKER_COMMAND}${MSG_END_FORMAT}\n\nsince the script is executed inside a docker container ... and starting Docker daemon inside a container is complicated to setup and overkill for our testing case."
-    else
-      print_msg "Execute command ${MSG_DIMMED_FORMAT}$ docker ${FULL_DOCKER_COMMAND}${MSG_END_FORMAT}"
+  else
+    print_msg "Execute command ${MSG_DIMMED_FORMAT}$ docker ${FULL_DOCKER_COMMAND}${MSG_END_FORMAT}"
 
-      # shellcheck disable=SC2086
-      docker ${FULL_DOCKER_COMMAND}
+    # shellcheck disable=SC2086
+    docker ${FULL_DOCKER_COMMAND}
 
-      print_msg_done "Command ${MSG_DIMMED_FORMAT}$ docker ${FULL_DOCKER_COMMAND}${MSG_END_FORMAT} completed and exited docker."
-    fi
+    print_msg_done "Command ${MSG_DIMMED_FORMAT}$ docker ${FULL_DOCKER_COMMAND}${MSG_END_FORMAT} completed and exited docker."
+  fi
+}
+
+# =================================================================================================================
+# Send TeamCity blockOpened/blockClosed service message or print the message to console
+#   when executed outside a TeamCity Agent run.
+#
+# Usage:
+#   $ teamcity_service_msg_blockOpened "<theMessage>"
+#   $ ... many steps ...
+#   $ teamcity_service_msg_blockClosed
+#
+# Globals:
+#   Read        'IS_TEAMCITY_RUN'
+#   Read|write  'CURRENT_BLOCK_SERVICE_MSG'
+# Outputs:
+#   Output either
+#     - a TeamCity blockOpened service message
+#     - or print to console
+#     - or an error if teamcity_service_msg_blockOpened is not closed using teamcity_service_msg_blockClosed
+#
+# =================================================================================================================
+# ....BLOC service message..........................................................................................
+function teamcity_service_msg_blockOpened() {
+  local THE_MSG=$1
+  if [[ ${CURRENT_BLOCK_SERVICE_MSG} ]]; then
+    print_msg_error_and_exit "The TeamCity bloc service message ${MSG_DIMMED_FORMAT}${CURRENT_BLOCK_SERVICE_MSG}${MSG_END_FORMAT} was not closed using function ${MSG_DIMMED_FORMAT}teamcity_service_msg_blockClosed${MSG_END_FORMAT}."
+  else
+    export CURRENT_BLOCK_SERVICE_MSG="${THE_MSG}"
+  fi
+
+  if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+    echo "##teamcity[blockOpened name='${MSG_BASE_TEAMCITY} ${THE_MSG}']"
+  else
+    echo && print_msg "${THE_MSG}" && echo
+  fi
+}
+
+function teamcity_service_msg_blockClosed() {
+  if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+    echo "##teamcity[blockClosed name='${MSG_BASE_TEAMCITY} ${CURRENT_BLOCK_SERVICE_MSG}']"
+  fi
+  # Reset the variable since the bloc is closed
+  unset CURRENT_BLOCK_SERVICE_MSG
+}
+
+# ....COMPILATION service message..................................................................................
+function teamcity_service_msg_compilationStarted() {
+  local THE_MSG=$1
+  if [[ ${CURRENT_COMPILATION_SERVICE_MSG_COMPILER} ]]; then
+    print_msg_error_and_exit "The TeamCity compilation service message ${MSG_DIMMED_FORMAT}${CURRENT_COMPILATION_SERVICE_MSG_COMPILER}${MSG_END_FORMAT} was not closed using function ${MSG_DIMMED_FORMAT}teamcity_service_msg_compilationFinished${MSG_END_FORMAT}."
+  else
+    export CURRENT_COMPILATION_SERVICE_MSG_COMPILER="${THE_MSG}"
+  fi
+
+  if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+    echo "##teamcity[compilationStarted compiler='${MSG_BASE_TEAMCITY} ${THE_MSG}']"
+  else
+    echo && print_msg "${THE_MSG}" && echo
+  fi
+}
+
+function teamcity_service_msg_compilationFinished() {
+  if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+    echo "##teamcity[compilationFinished compiler='${MSG_BASE_TEAMCITY} ${CURRENT_COMPILATION_SERVICE_MSG_COMPILER}']"
+  fi
+  # Reset the variable since the bloc is closed
+  unset CURRENT_COMPILATION_SERVICE_MSG_COMPILER
 }
 
--- build_system/ubuntu/lpm_install_dependencies_ubuntu.bash
+++ build_system/ubuntu/lpm_install_dependencies_ubuntu.bash
@@ -1,6 +1,5 @@
 #!/bin/bash -i
 #
-#
 set -e
 #set -v
 
@@ -25,6 +24,7 @@ export DEBIAN_FRONTEND=noninteractive
 # import shell functions from Libpointmatcher-build-system utilities library
 source ./function_library/prompt_utilities.bash
 source ./function_library/terminal_splash.bash
+source ./function_library/general_utilities.bash
 
 # Set environment variable LPM_IMAGE_ARCHITECTURE
 source ./lpm_utility_script/lpm_export_which_architecture.bash
@@ -39,13 +39,14 @@ fi
 print_formated_script_header "lpm_install_dependencies_ubuntu.bash (${LPM_IMAGE_ARCHITECTURE})" "${LPM_LINE_CHAR_INSTALLER}"
 
 # ................................................................................................................
-if [[ ${IS_TEAMCITY_RUN} == true ]]; then
-  echo "##teamcity[blockOpened name='${MSG_BASE_TEAMCITY} Install development utilities']"
-else
-  echo
-  print_msg "Install development utilities"
-  echo
-fi
+teamcity_service_msg_block_open "Install development utilities"
+#if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+#  echo "##teamcity[blockOpened name='${MSG_BASE_TEAMCITY} Install development utilities']"
+#else
+#  echo
+#  print_msg "Install development utilities"
+#  echo
+#fi
 
 sudo apt-get update &&
   sudo apt-get install --assume-yes \
@@ -64,14 +65,16 @@ sudo apt-get update &&
 
 cmake --version
 
-if [[ ${IS_TEAMCITY_RUN} == true ]]; then
-  echo "##teamcity[blockClosed name='${MSG_BASE_TEAMCITY} Install development utilities']"
-fi
+teamcity_service_msg_block_close
+#if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+#  echo "##teamcity[blockClosed name='${MSG_BASE_TEAMCITY} Install development utilities']"
+#fi
 
 # ................................................................................................................
-if [[ ${IS_TEAMCITY_RUN} == true ]]; then
-  echo "##teamcity[blockOpened name='${MSG_BASE_TEAMCITY} Install development utilities (python)']"
-fi
+teamcity_service_msg_block_open "Install development utilities (python)"
+#if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+#  echo "##teamcity[blockOpened name='${MSG_BASE_TEAMCITY} Install development utilities (python)']"
+#fi
 
 source ./ubuntu/lpm_install_python_dev_tools.bash
 
@@ -84,122 +87,137 @@ source ./ubuntu/lpm_install_python_dev_tools.bash
 #        python3-vcstool \
 #    && sudo rm -rf /var/lib/apt/lists/*;
 
-if [[ ${IS_TEAMCITY_RUN} == true ]]; then
-  echo "##teamcity[blockClosed name='${MSG_BASE_TEAMCITY} Install development utilities (python)']"
-fi
+teamcity_service_msg_block_close
+#if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+#  echo "##teamcity[blockClosed name='${MSG_BASE_TEAMCITY} Install development utilities (python)']"
+#fi
 
 
 # ................................................................................................................
 # https://www.boost.org/doc/libs/1_79_0/more/getting_started/unix-variants.html
-if [[ ${IS_TEAMCITY_RUN} == true ]]; then
-  echo "##teamcity[blockOpened name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dependencies › Boost']"
-else
-  echo
-  print_msg "Install Libpointmatcher dependencies › 'Boost'"
-  echo
-fi
+teamcity_service_msg_block_open "Install Libpointmatcher dependencies › Boost"
+#if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+#  echo "##teamcity[blockOpened name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dependencies › Boost']"
+#else
+#  echo
+#  print_msg "Install Libpointmatcher dependencies › 'Boost'"
+#  echo
+#fi
 
 sudo apt-get update &&
   sudo apt-get install --assume-yes \
     libboost-all-dev &&
   sudo rm -rf /var/lib/apt/lists/*
 
-if [[ ${IS_TEAMCITY_RUN} == true ]]; then
-  echo "##teamcity[blockClosed name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dependencies › Boost']"
-fi
+teamcity_service_msg_block_close
+#if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+#  echo "##teamcity[blockClosed name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dependencies › Boost']"
+#fi
+
+# ................................................................................................................
+# https://eigen.tuxfamily.org/index.php
+teamcity_service_msg_block_open "Install Libpointmatcher dependencies › Eigen"
+#if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+#  echo "##teamcity[blockOpened name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dependencies › Eigen']"
+#else
+#  echo
+#  print_msg "Install Libpointmatcher dependencies › 'Eigen'"
+#  echo
+#fi
+
+sudo apt-get update &&
+  sudo apt-get install --assume-yes \
+    libeigen3-dev &&
+  sudo rm -rf /var/lib/apt/lists/*
+
+teamcity_service_msg_block_close
+#if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+#  echo "##teamcity[blockClosed name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dependencies › Eigen']"
+#fi
 
 
-## ................................................................................................................
-#echo
-#print_msg "Install Libpointmatcher dependencies › 'Eigen'"
-#if [[ ${IS_TEAMCITY_RUN} == true ]]; then echo "##teamcity[blockOpened name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dependencies › Eigen']"; fi
-#echo
-## https://eigen.tuxfamily.org/index.php
-#
-#
-#sudo apt-get update &&
-#  sudo apt-get install --assume-yes \
-#    libeigen3-dev &&
-#  sudo rm -rf /var/lib/apt/lists/*
-#
-#if [[ ${IS_TEAMCITY_RUN} == true ]]; then echo "##teamcity[blockClosed name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dependencies › Eigen']"; fi
-#
-## ................................................................................................................
-#print_msg "Create required dir structure"
-#mkdir -p "${LPM_INSTALLED_LIBRARIES_PATH}"
-#
-## ................................................................................................................
-#echo
-#print_msg "Install Libpointmatcher dependencies › 'Libnabo'"
-#if [[ ${IS_TEAMCITY_RUN} == true ]]; then echo "##teamcity[blockOpened name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dependencies › Libnabo']"; fi
-#echo
-## https://github.com/ethz-asl/libnabo
-#
-#
-## ToDo: assessment >> ANN and FLANN should be required only for `make test` benchmarks
-#
-### Note:ANN was not mentionned in doc --> probably because it's only used in benchmark test
-### ANN is a library written in C++, which supports data structures and algorithms for both exact and approximate nearest neighbor searching in arbitrarily high dimensions.
-### https://www.cs.umd.edu/~mount/ANN/
-##cd "${LPM_INSTALLED_LIBRARIES_PATH}"
-##wget https://www.cs.umd.edu/~mount/ANN/Files/1.1.2/ann_1.1.2.tar.gz
-##tar xzf ann_1.1.2.tar.gz
-##cd ann_1.1.2/
-##make linux-g++
-##sudo cp lib/libANN.a /usr/local/lib/
-##sudo cp include/ANN/ANN.h /usr/local/include/
-### shellcheck disable=SC2103
-##cd ..
-##
-##
-### Note:FLANN was not mentionned in doc --> probably because it's only used in benchmark test
-### Fast Library for Approximate Nearest Neighbors - development
-### FLANN is a library for performing fast approximate nearest neighbor searches in high dimensional spaces.
-### https://github.com/flann-lib/flann
-##sudo apt-get update \
-##    && sudo apt-get install --assume-yes \
-##        libflann-dev \
-##    && sudo rm -rf /var/lib/apt/lists/*
-#
+# ................................................................................................................
+# https://github.com/ethz-asl/libnabo
+teamcity_service_msg_block_open "Install Libpointmatcher dependencies › Libnabo"
+#if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+#  echo "##teamcity[blockOpened name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dependencies › Libnabo']"
+#else
+#  echo
+#  print_msg "Install Libpointmatcher dependencies › 'Libnabo'"
+#  echo
+#fi
+
+print_msg "Create required dir structure"
+mkdir -p "${LPM_INSTALLED_LIBRARIES_PATH}"
+
+# ToDo: assessment >> ANN and FLANN should be required only for `make test` benchmarks
+
+## Note:ANN was not mentionned in doc --> probably because it's only used in benchmark test
+## ANN is a library written in C++, which supports data structures and algorithms for both exact and approximate nearest neighbor searching in arbitrarily high dimensions.
+## https://www.cs.umd.edu/~mount/ANN/
 #cd "${LPM_INSTALLED_LIBRARIES_PATH}"
-#git clone https://github.com/ethz-asl/libnabo.git &&
-#  cd libnabo &&
-#  mkdir build && cd build
+#wget https://www.cs.umd.edu/~mount/ANN/Files/1.1.2/ann_1.1.2.tar.gz
+#tar xzf ann_1.1.2.tar.gz
+#cd ann_1.1.2/
+#make linux-g++
+#sudo cp lib/libANN.a /usr/local/lib/
+#sudo cp include/ANN/ANN.h /usr/local/include/
+## shellcheck disable=SC2103
+#cd ..
 #
 #
+## Note:FLANN was not mentionned in doc --> probably because it's only used in benchmark test
+## Fast Library for Approximate Nearest Neighbors - development
+## FLANN is a library for performing fast approximate nearest neighbor searches in high dimensional spaces.
+## https://github.com/flann-lib/flann
+#sudo apt-get update \
+#    && sudo apt-get install --assume-yes \
+#        libflann-dev \
+#    && sudo rm -rf /var/lib/apt/lists/*
+
+cd "${LPM_INSTALLED_LIBRARIES_PATH}"
+git clone https://github.com/ethz-asl/libnabo.git &&
+  cd libnabo &&
+  mkdir build && cd build
+
+# git checkout 1.0.7
+
+teamcity_service_msg_compilationStarted "cmake"
 #if [[ ${IS_TEAMCITY_RUN} == true ]]; then echo "##teamcity[compilationStarted compiler='${MSG_BASE_TEAMCITY} cmake']"; fi
-#
-#cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo .. &&
-#  make -j $(nproc) &&
-##  make test &&              # (CRITICAL) ToDo: on task end >> unmute this line ←
-#  sudo make install
-#
+
+cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo .. &&
+  make -j $(nproc) &&
+#  make test &&              # (CRITICAL) ToDo: on task end >> unmute this line ←
+  sudo make install
+
+teamcity_service_msg_compilationFinished
 #if [[ ${IS_TEAMCITY_RUN} == true ]]; then echo "##teamcity[compilationFinished compiler='${MSG_BASE_TEAMCITY} cmake']"; fi
-#
-#
-##    && git checkout 1.0.7 \
-#
-## ToDo:on task end >> next bloc ↓↓
-##pwd && tree -L 3
-#
-#if [[ ${IS_TEAMCITY_RUN} == true ]]; then echo "##teamcity[blockClosed name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dependencies › Libnabo']"; fi
-#
-## ................................................................................................................
-#echo
-#print_msg "Install libpointmatcher dev tools"
-#if [[ ${IS_TEAMCITY_RUN} == true ]]; then echo "##teamcity[blockOpened name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dev tools']"; fi
-#echo
-#
-#sudo apt-get update &&
-#  sudo apt-get install --assume-yes \
-#    libyaml-cpp-dev &&
-#  sudo rm -rf /var/lib/apt/lists/*
-#
-#if [[ ${IS_TEAMCITY_RUN} == true ]]; then echo "##teamcity[blockClosed name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dev tools']"; fi
 
+teamcity_service_msg_block_close
+#if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+#  echo "##teamcity[blockClosed name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dependencies › Libnabo']"
+#fi
 
-## Tag added to the TeamCity build via a service message
-#echo "##teamcity[addBuildTag '${LPM_IMAGE_ARCHITECTURE}']"
+# ................................................................................................................
+teamcity_service_msg_block_open "Install Libpointmatcher dev tools"
+#if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+#  echo "##teamcity[blockOpened name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dev tools']"
+#else
+#  echo
+#  print_msg "Install libpointmatcher dev tools"
+#  echo
+#fi
+
+sudo apt-get update &&
+  sudo apt-get install --assume-yes \
+    libyaml-cpp-dev &&
+  sudo rm -rf /var/lib/apt/lists/*
+
+teamcity_service_msg_block_close
+#if [[ ${IS_TEAMCITY_RUN} == true ]]; then
+#  echo "##teamcity[blockClosed name='${MSG_BASE_TEAMCITY} Install Libpointmatcher dev tools']"
+#fi
+
 
 print_msg_done "Libpointmatcher dependencies installed"
 print_formated_script_footer "lpm_install_dependencies_ubuntu.bash (${LPM_IMAGE_ARCHITECTURE})" "${LPM_LINE_CHAR_INSTALLER}"
