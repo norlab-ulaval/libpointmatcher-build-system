67850ddc2a456beef8b20758f74fd361687266d4
RedLeaderOne <luc.coupal.1@ulaval.ca>
2023-08-04 17:57:45 -0400

    NMO-193 | Fix pip install related steps in lpm_install_python_dev_tools.bash;

--- build_system/ubuntu/lpm_install_python_dev_tools.bash
+++ build_system/ubuntu/lpm_install_python_dev_tools.bash
@@ -22,14 +22,13 @@ source ./function_library/general_utilities.bash
 # ====Install python version based on ubuntu distro================================================================
 if [[ $(uname) == 'Linux' ]]; then
 
-  teamcity_service_msg_blockOpened "Install python development tools"
-
   # Retrieve ubuntu version number
   UBUNTU_VERSION=$(grep '^VERSION_ID' /etc/os-release)
   if [[ ${UBUNTU_VERSION} == '18.04' ]]; then
-    # Case: Ubuntu 18.04 (bionic) ==> python 2
 
     # ....Case › Ubuntu bionic ==> python 2........................................................................
+    teamcity_service_msg_blockOpened "Install python development tools for Ubuntu distro (python 2)"
+
     sudo apt-get update &&
       sudo apt-get install --assume-yes \
         python-dev \
@@ -40,33 +39,40 @@ if [[ $(uname) == 'Linux' ]]; then
     curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
     sudo python2 get-pip.py
 
+    teamcity_service_msg_blockClosed
   else
+    teamcity_service_msg_blockOpened "Install python development tools for Ubuntu distro (python 3)"
 
-    # ....Case › Ubuntu distro still have to deal with python 2 legacy code........................................
     if [[ ${UBUNTU_VERSION} == '20.04' ]]; then
+      # ....Case › Ubuntu distro still have to deal with python 2 legacy code.....................................
       sudo apt-get update &&
         sudo apt-get install --assume-yes \
+          python3-dev \
+          python3-pip \
+          python3-numpy \
           python-is-python3 &&
+        sudo rm -rf /var/lib/apt/lists/*
+    else
+      # ....Case › python 3 is Ubuntu default python version......................................................
+      sudo apt-get update &&
+        sudo apt-get install --assume-yes \
+          python3-dev \
+          python3-pip \
+          python3-numpy &&
         sudo rm -rf /var/lib/apt/lists/*
     fi
 
-    # ....Case › python 3 is Ubuntu default python version.........................................................
-    sudo apt-get update &&
-      sudo apt-get install --assume-yes \
-        python3-dev \
-        python3-pip \
-        python3-numpy &&
-      sudo rm -rf /var/lib/apt/lists/*
-
-    # python3-opengl \
-
-    # python3 -m pip install --upgrade pip
-    pip install --no-cache-dir --upgrade pip
-
+    teamcity_service_msg_blockClosed
   fi
-  teamcity_service_msg_blockClosed
 fi
 
+# ====Universal python install step (common to all OS version)======================================================
+teamcity_service_msg_blockOpened "Upgrade pip"
+
+# python3 -m pip install --upgrade pip
+pip install --no-cache-dir --upgrade pip
+
+teamcity_service_msg_blockClosed
 # ====Install TeamCity and CI/CD utilities==========================================================================
 if [[ ${IS_TEAMCITY_RUN} == true ]]; then
   teamcity_service_msg_blockOpened "Install TeamCity and CI/CD python tools"
